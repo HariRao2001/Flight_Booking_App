197ebcfa0276a4b77de1a8ffb8968ee23fdd69ef
HariRao2001 <160570873+HariRao2001@users.noreply.github.com>
2025-08-12 19:33:10 +0530

    Update PageNotFound.jsx

--- src/assets/components/PageNotFound/PageNotFound.jsx
+++ src/assets/components/PageNotFound/PageNotFound.jsx
@@ -2,6 +2,6 @@ export default function PageNotFound(){
     return <div className="page_not_found_block">
         <h1>404</h1>
         <h2>Oops! Page Not Found</h2>
-        <p>Uh oh, We can't seem to find the page you are looking for.Try going back to the previous page</p>
+        <p>Uh oh, We can't seem to find the page you are looking for.Try again by move back to the previous page</p>
     </div>
 }
